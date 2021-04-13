bb9d0b6c4e15712a2c90abbc7c388555e7a84a39
VanessaStoellinger <vanessa.stoellinger@htl-salzburg.ac.at>
2021-04-13 09:23:33 +0200

    UI Design and Function

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void ChangeScene2()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+}
